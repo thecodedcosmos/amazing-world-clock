4d37bc152fd7410992220088f1b3b1e8d7193abc
Adrianna Bell <120890577+thecodedcosmos@users.noreply.github.com>
2023-07-24 13:41:23 -0700

    Added a current location select to html and automated it through JS.

--- js/index.js
+++ js/index.js
@@ -20,6 +20,9 @@ rabatTimeElement.innerHTML = `${rabatTime.format("h:mm:ss")} <small>${rabatTime.
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", "").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
